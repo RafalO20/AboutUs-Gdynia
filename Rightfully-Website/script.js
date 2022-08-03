330fcecbb00f8e76ae47cda224cde9bd085c64c3
RafalO20 <72500457+RafalO20@users.noreply.github.com>
2022-08-03 09:01:06 +0200

    Rightfully-Website

---
+++ Rightfully-Website/script.js
@@ -0,0 +1,15 @@
+const hamburger = document.getElementById('hamburger');
+const navUl = document.getElementById('nav-ul');
+
+hamburger.addEventListener('click', () => {
+    navUl.classList.toggle('show');
+});
+
+const accordion = document.getElementsByClassName('frequentyl-section__box');
+for (i = 0; i<accordion.length; i++){
+    accordion[i].addEventListener('click', function(){
+        this.classList.toggle('active')
+    })
+}
+
+
