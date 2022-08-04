8fa6517c56935b38005d20ef92535e9860eec3b7
RafalO20 <72500457+RafalO20@users.noreply.github.com>
2022-08-04 08:48:16 +0200

    Add files via upload

--- Rightfully-Website/script.js
+++ Rightfully-Website/script.js
@@ -12,4 +12,41 @@ for (i = 0; i<accordion.length; i++){
     })
 }
 
+var slideIndex = 1;
+showDivs(slideIndex);
 
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("portal__slider");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+function showDivsTwo(n) {
+  var i;
+  var x = document.getElementsByClassName("portal__slider--two");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
