e4a04b8e0123caab0f784ccea208ab2bf5707352
RafalO20 <72500457+RafalO20@users.noreply.github.com>
2022-08-04 08:59:31 +0200

    Add files via upload

--- Rightfully-Website/script.js
+++ Rightfully-Website/script.js
@@ -35,18 +35,25 @@ function showDivs(n) {
   dots[slideIndex-1].className += " active";
 }
 
+var slideIndexTwo = 1;
+showDivsTwo(slideIndexTwo);
+
+function currentDivTwo(n) {
+  showDivsTwo(slideIndexTwo = n);
+}
+
 function showDivsTwo(n) {
   var i;
   var x = document.getElementsByClassName("portal__slider--two");
   var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
+  if (n > x.length) {slideIndexTwo = 1}    
+  if (n < 1) {slideIndexTwo = x.length}
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  x[slideIndexTwo-1].style.display = "block";  
+  dots[slideIndexTwo-1].className += " active";
 }
